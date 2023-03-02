6dffbaf613e309b146077cff98451450164828c6
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-03-02 14:06:54 +0100

    Added if to change page icon based on user theme

--- src/main.js
+++ src/main.js
@@ -3,3 +3,11 @@ import './style.css';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+// Handle change of icon based on user theme
+
+const link = document.querySelector('link[rel="icon"]');
+
+if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    link.href = '/video-player-icon-dark.png';
+}
