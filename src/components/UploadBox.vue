6c60590ed4ceb31c04ab97a2d9195bcc501e3159
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-03-04 16:57:07 +0100

    Finished UploadBox component

---
+++ src/components/UploadBox.vue
@@ -0,0 +1,227 @@
+<script setup>
+import { ref, unref, onMounted } from 'vue';
+
+const uploadBox = ref('');
+const uploadVideoFile = ref('');
+const uploadBoxText = ref('');
+
+const videoSrcEmit = defineEmits(['videoSrc']);
+
+const initUploadBoxEvents = () => {
+    const unrefUploadBox = unref(uploadBox),
+          unrefUploadVideoFile = unref(uploadVideoFile),
+          unrefUploadBoxText = unref(uploadBoxText),
+
+          validVideoFormat = ['m4v', 'avi', 'mpg', 'mp4'],
+          hoverClassName = 'hovered',
+          errorAnimationName = 'animate__shakeX',
+          moveLeftAnimationName = 'animate__backOutLeft';
+    
+
+    // Function used to convert a file into base64
+    const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+
+    /* 
+        Function that converts video to base64 by calling
+        the previously defined function
+    */
+    const convertVideoToBase64 = async () => {
+        try {
+            const result = await toBase64(unrefUploadVideoFile.files[0]);
+            videoSrcEmit('videoSrc', result);
+            unrefUploadBox.classList.add(moveLeftAnimationName);
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    /* 
+        Function used to check is the uploaded video
+        following certain guidelines, those are:
+        - that only a single file is uploaded
+        - that the single file is a video
+    */
+    const uploadedVideoCheck = () => {
+        const fileExtension = unrefUploadVideoFile.files[0].name.split('.')[1];
+
+        if(unrefUploadVideoFile.files.length !== 1) {
+            unrefUploadBoxText.innerText = 'Too many files! Upload a video!';
+            unrefUploadBox.classList.add(errorAnimationName);
+        } else if(!validVideoFormat.includes(fileExtension)) {
+            unrefUploadBoxText.innerText = 'Upload a video!';
+            unrefUploadBox.classList.add(errorAnimationName);
+        } else {
+            convertVideoToBase64();
+        }
+    }
+
+    unrefUploadBox.addEventListener('animationend', () => {
+        unrefUploadBox.classList.remove(errorAnimationName);
+    });
+
+    unrefUploadBox.addEventListener('click', () => {
+        unrefUploadVideoFile.click();
+    });
+
+    unrefUploadBox.addEventListener('dragover', e => {
+        e.preventDefault();
+
+        unrefUploadBox.classList.add(hoverClassName);
+    });
+
+    unrefUploadBox.addEventListener('dragleave', () => {
+        unrefUploadBox.classList.remove(hoverClassName);
+    });
+
+    unrefUploadBox.addEventListener('drop', e => {
+        e.preventDefault();
+
+        unrefUploadBox.classList.remove(hoverClassName);
+        unrefUploadVideoFile.files = e.dataTransfer.files;
+        uploadedVideoCheck();
+    });
+
+    unrefUploadVideoFile.addEventListener('change', () => {
+        uploadedVideoCheck();
+    });
+}
+
+onMounted(initUploadBoxEvents);
+
+</script>
+
+<template>
+    <button class="upload-box animate__animated" ref="uploadBox">
+        <h2 class="upload-box-text" ref="uploadBoxText">Upload Video Here</h2>
+        <div class="upload-box-x">
+            <div class="box"></div>
+        </div>
+        <input type="file" name="uploadVideoFile" id="uploadVideoFile" ref="uploadVideoFile" required>
+    </button>
+</template>
+
+<style scoped>
+    .upload-box {
+        /* 
+            Variable used to control the width of:
+            - the upload box
+            - the box with the + inside
+            - the lines making up the +
+        */
+        --additional-size: 2px;
+        --animation-duration: 1s;
+
+        width: min(60%, 600px);
+        height: min(40%, 400px);
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        translate: -50% -50%;
+
+        background: none;
+
+        cursor: pointer;
+
+        animation-duration: var(--animation-duration);  
+    }
+    
+    .upload-box::before {
+        --blur-size: blur(20px);
+
+        content: '';
+        width: calc(100% + var(--additional-size));
+        height: calc(100% + var(--additional-size));
+
+        position: absolute;
+        top: calc(var(--additional-size) / -2);
+        left: calc(var(--additional-size) / -2);
+
+        background-color: white;
+
+        z-index: 0;
+
+        transition: filter .5s ease;
+    }
+
+    .upload-box:hover::before,
+    .upload-box.hovered::before {
+        filter: var(--blur-size);
+    }
+
+    .upload-box-text {
+        color: white;
+        font-size: 4vmin;
+        width: 100%;
+
+        position: absolute;
+        top: 15%;
+        left: 50%;
+        translate: -50% -20%;
+
+        z-index: 2;
+    }
+
+    .upload-box-x {
+        width: 100%;
+        height: 100%;
+
+        position: absolute;
+        inset: 0;
+
+        background-color: var(--app-bg-color);
+
+        z-index: 1;
+    }
+
+    .box {
+        width: 50%;
+        height: 50%;
+
+        position: absolute;
+        top: 70%;
+        left: 50%;
+        translate: -50% -70%;
+
+        border: 4px dashed white;
+
+        transition: border-style 1s ease;
+    }
+
+    .upload-box:hover .box,
+    .upload-box.hovered .box {
+        border-style: solid;
+    }
+
+    .box::before,
+    .box::after {
+        content: '';
+
+        width: 30%;
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        translate: -50% -50%;
+
+        border: var(--additional-size) solid white;
+    }
+
+    .box::after {
+        top: 48%;
+        rotate: 90deg;
+    }
+
+    #uploadVideoFile {
+        position: absolute;
+        inset: 0;
+
+        pointer-events: none;
+        visibility: hidden;
+    }
+</style>
