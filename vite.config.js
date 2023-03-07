ed668ed54ea66969365964cb5513ad11df1df7da
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-03-07 23:32:12 +0100

    Updated base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/video-loader-and-player/'
+  base: '/vuejs-video-loader-and-player/'
 })
